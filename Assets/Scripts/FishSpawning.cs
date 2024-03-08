a66e9a12f463e7a20280848585802c6e197f9012
obamagaming <91400278+brassmaster2357@users.noreply.github.com>
2024-03-08 10:24:21 -0800

    CHEWSDAY INNIT????

--- Assets/Scripts/FishSpawning.cs
+++ Assets/Scripts/FishSpawning.cs
@@ -6,14 +6,22 @@ public class FishSpawning : MonoBehaviour
 {
     public GameObject[] fishTypes;
 
-    void Start()
+    private void Start()
     {
-        
+        for (int i = 0; i < 100; i++)
+        {
+            SpawnFish(-i);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    void SpawnFish(float depth)
     {
-        
+        float randomFishIndex = depth + (Mathf.Sqrt(Random.Range(0,9f) * Random.Range(-1,2)));
+        Debug.Log(randomFishIndex);
+        randomFishIndex = Mathf.RoundToInt(Mathf.Clamp(randomFishIndex, 0, fishTypes.Length - 1));
+        Debug.Log(randomFishIndex);
+        Debug.Log((int)randomFishIndex);
+        GameObject fish = Instantiate(fishTypes[(int)randomFishIndex]);
+        fish.transform.position = new Vector3(Random.Range(-15f, 15f), Mathf.Clamp(depth + Random.Range(-3f, 3f),-9999999,-1));
     }
 }
