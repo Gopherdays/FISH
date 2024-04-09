92908def396267be426c50b738310a1a30f26d62
obamagaming <91400278+brassmaster2357@users.noreply.github.com>
2024-04-08 20:31:14 -0700

    FISH FLIP
    whOOOAHOAHOHAAHHAHHHHHHHH

--- Assets/Scripts/Fish.cs
+++ Assets/Scripts/Fish.cs
@@ -4,28 +4,19 @@ using UnityEngine;
 
 public class Fish : MonoBehaviour
 {
-    public float speed;
-    public int randomness;
-
-    WaterPhysics waterPhysics;
     Rigidbody2D rb;
     Vector2 velocityVector;
     public float swimSpeed;
     public float swimCycle = 0.2f;
-    float randomOffset;
-    public float bounceSpeed = 4;
     public Vector2 hookPoint;
 
     void Start()
     {
-        waterPhysics = GetComponent<WaterPhysics>();
         rb = GetComponent<Rigidbody2D>();
-        randomOffset = Random.Range(0, Mathf.PI * 2);
     }
 
     void Update()
     {
-        waterPhysics.waterGravity = Mathf.Sin(randomOffset + Time.time * bounceSpeed) * swimCycle;
         velocityVector = rb.velocity;
         velocityVector.x = swimSpeed;
         rb.velocity = velocityVector;
--- Assets/Scripts/FishSpawning.cs
+++ Assets/Scripts/FishSpawning.cs
@@ -8,7 +8,7 @@ public class FishSpawning : MonoBehaviour
 
     private void Start()
     {
-        for (int i = 3; i < 1000; i++)
+        for (int i = 3; i < 1000; i += 2)
         {
             SpawnFish(-i);
         }
@@ -16,9 +16,17 @@ public class FishSpawning : MonoBehaviour
 
     void SpawnFish(float depth)
     {
-        float randomFishIndex = (-depth + Mathf.Sqrt(Random.Range(0,9f) * Random.Range(0,2))) / 10;
+        float randomFishIndex = ((-depth/2) + Mathf.Sqrt(Random.Range(0,9f) * Random.Range(0,2))) / 10; // uuuummm... you know maybe I should turn off my math brain and think simpler every once in a while
         randomFishIndex = Mathf.RoundToInt(Mathf.Clamp(randomFishIndex, 0, fishTypes.Length - 1));
         GameObject fish = Instantiate(fishTypes[(int)randomFishIndex]);
-        fish.transform.position = new Vector3(Random.Range(-45f, 45f), Mathf.Clamp(depth + Random.Range(-3f, 3f),-9999999,-1));
+        fish.transform.position = new Vector3(Random.Range(-85f, 85f), Mathf.Clamp(depth + Random.Range(-3f, 3f),-9999999,-1));
+        fish.GetComponent<Fish>().swimSpeed *= Random.Range(0.6f, 1.35f);
+        if (Random.Range(0,2) == 0)
+        {
+            Vector3 temp = fish.transform.localScale;
+            temp.x *= -1;
+            fish.transform.localScale = temp;
+            fish.GetComponent<Fish>().swimSpeed *= -1;
+        }
     }
 }
