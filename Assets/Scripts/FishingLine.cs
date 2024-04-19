cb06a3a97c79362061be0456e5f2cd7402af9439
obamagaming <91400278+brassmaster2357@users.noreply.github.com>
2024-04-18 19:20:40 -0700

    Demo assets added and stuff

--- Assets/Scripts/FishingLine.cs
+++ Assets/Scripts/FishingLine.cs
@@ -6,7 +6,9 @@ public class FishingLine : MonoBehaviour
 {
     LineRenderer line;
     public GameObject origin;
+    public Vector2 offsetOrigin;
     public GameObject hook;
+    public Vector2 offsetHook;
 
     Vector3[] positions = new Vector3[3];
 
@@ -17,11 +19,10 @@ public class FishingLine : MonoBehaviour
 
     void Update()
     {
-        positions[0] = origin.transform.position;
-        positions[1] = hook.transform.position;
-        positions[2] = hook.transform.position;
-        positions[2].y += 0.45f;
-        if (hook.transform.position.y < -0.45f)
+        positions[0] = origin.transform.position + (Vector3)offsetOrigin;
+        positions[1] = positions[2] = hook.transform.position;
+        positions[2] += (Vector3)offsetHook;
+        if (hook.transform.position.y < offsetHook.y)
             positions[1].y = 0;
         else
             positions[1] = positions[2];
