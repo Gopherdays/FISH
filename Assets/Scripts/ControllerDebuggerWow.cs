a7e5a65715204992d6ad9c36d2ac70b00efeecf9
obamagaming <91400278+brassmaster2357@users.noreply.github.com>
2024-04-26 08:13:45 -0700

    Update ControllerDebuggerWow.cs

--- Assets/Scripts/ControllerDebuggerWow.cs
+++ Assets/Scripts/ControllerDebuggerWow.cs
@@ -7,9 +7,28 @@ public class ControllerDebuggerWow : MonoBehaviour
 {
     InputDevice device;
 
+    private InputDevice FindJoystick()
+    {
+        for (int i = 0; i < 100; i++)
+        {
+            if (InputSystem.devices.Count <= i)
+            {
+                print("HELP HELP HELP");
+                return null;
+            }
+            InputDevice test = InputSystem.GetDeviceById(i);
+            if (test.name.Contains("Microntek"))
+            {
+                return InputSystem.GetDevice(test.name);
+            }
+        }
+        return null;
+    }
+
     private void Update()
     {
-        device = InputSystem.GetDeviceById(17);
+        if (!device.name.Contains("Microntek"))
+            device = FindJoystick();
         print(device["stick"].ReadValueAsObject());
         print(device["trigger"].ReadValueAsObject());
     }
